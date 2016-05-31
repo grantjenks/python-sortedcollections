f090171854e024bf0fa897ecccc05129ebc8787c
Grant Jenks <contact@grantjenks.com>
2016-05-30 18:14:28 -0700

    Bump version to 0.4.1

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-
-import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
+import sys
+
 
 class Tox(TestCommand):
     def finalize_options(self):
@@ -14,21 +13,19 @@ class Tox(TestCommand):
         errno = tox.cmdline(self.test_args)
         sys.exit(errno)
 
+
 with open('README.rst') as fptr:
     readme = fptr.read()
 
-with open('LICENSE') as fptr:
-    license = fptr.read()
-
 setup(
     name='sortedcollections',
-    version='0.3.1',
+    version='0.4.1',
     description='Python Sorted Collections',
     long_description=readme,
     author='Grant Jenks',
     author_email='contact@grantjenks.com',
     url='http://www.grantjenks.com/docs/sortedcollections/',
-    license=license,
+    license='Apache 2.0',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=['sortedcontainers'],
     tests_require=['tox'],
@@ -36,16 +33,18 @@ setup(
     classifiers=(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ),
 )
