3582446908d75d7eb7947698afcc4b0ba5c8a2e6
Grant Jenks <contact@grantjenks.com>
2016-05-30 17:30:12 -0700

    Update copyright years: 2015-2016

--- README.rst
+++ README.rst
@@ -46,7 +46,7 @@ Reference and Indices
 SortedCollections License
 -------------------------
 
-Copyright 2015 Grant Jenks
+Copyright 2015-2016 Grant Jenks
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- sortedcollections/__init__.py
+++ sortedcollections/__init__.py
@@ -17,8 +17,8 @@ from .recipes import OrderedSet, SegmentList
 from .ordereddict import OrderedDict
 
 __title__ = 'sortedcollections'
-__version__ = '0.3.1'
-__build__ = 0x000301
+__version__ = '0.4.0'
+__build__ = 0x000400
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2016 Grant Jenks'
+__copyright__ = 'Copyright 2015-2016 Grant Jenks'
