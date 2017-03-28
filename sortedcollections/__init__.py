a5161b66e029aa25f2beff18ce4be58e242e0936
Grant Jenks <contact@grantjenks.com>
2017-03-27 22:28:51 -0700

    Bump version to 0.5.1

--- sortedcollections/__init__.py
+++ sortedcollections/__init__.py
@@ -17,8 +17,8 @@ from .recipes import OrderedSet, SegmentList
 from .ordereddict import OrderedDict
 
 __title__ = 'sortedcollections'
-__version__ = '0.5.0'
-__build__ = 0x000500
+__version__ = '0.5.1'
+__build__ = 0x000501
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2015-2016 Grant Jenks'
