5c383213e77be628e1f486ea5fe7a17fb1491495
Grant Jenks <grant.jenks@gmail.com>
2020-06-07 12:53:14 -0700

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.rst') as reader:
 
 setup(
     name='sortedcollections',
-    version='1.1.2',
+    version='1.2.1',
     description='Python Sorted Collections',
     long_description=readme,
     author='Grant Jenks',
